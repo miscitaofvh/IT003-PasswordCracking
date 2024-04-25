229cf531177c2a5584cb27979ee4f6c63cddb89e
miscitaofvh <nguyenvanhung14112005@gmail.com>
2024-04-25 09:42:06 +0700

    update root file and create other html file

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ document.querySelectorAll('.app-option').forEach(option => {
         option.classList.add('selected');
     });
 });
+
+const confirmButton = document.querySelector('.submit-button');
+
+confirmButton.addEventListener('click', () => {
+    const selectedApp = document.querySelector('.app-option.selected');
+    if (selectedApp) {
+        const appName = selectedApp.getAttribute('app-name');
+        switch (appName) {
+            case 'facebook':
+                window.location.href = 'facebook.html';
+                break;
+            case 'instagram':
+                window.location.href = 'instagram.html';
+                break;
+            case 'gmail':
+                window.location.href = 'gmail.html';
+                break;
+        }
+    } else {
+        console.log('Please choose an application');
+    }
+});
